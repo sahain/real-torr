f570d16ed7e989b6ea53fcf7a53c2470eb410b1c
Sam <sam@sam-Lenovo-G505s.(none)>
2014-08-15 16:50:23 -0700

    added image assets, seeds, prod env asset change for heroku, prop.desc helper

--- app/helpers/properties_helper.rb
+++ app/helpers/properties_helper.rb
@@ -7,7 +7,15 @@ module PropertiesHelper
   	if property.image_file_name.blank?
   	  image_tag('placeholder.png')
   	else
-  		image_tag(property.image_file_name)
+  	  image_tag(property.image_file_name)
+  	end
+  end
+
+  def description_for(property) # change text later to better suit app
+  	if property.description.blank?
+  		content_tag(:p, "Bacon ipsum dolor sit amet rump ground round jowl pork belly ham hock. Venison chuck fatback ball tip, drumstick jerky sausage ground round pig andouille pork belly. Ball tip spare ribs chicken prosciutto andouille ribeye. Sausage andouille tenderloin sirloin leberkas pastrami. Ham fatback venison swine ball tip short loin boudin corned beef turducken bacon prosciutto t-bone pork. Porchetta cow meatball turducken shank, biltong pig doner landjaeger venison leberkas tri-tip bresaola.")
+  	else
+  		content_tag(:p, property.description)
   	end
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Property.destroy_all
+Property.create!([
+	{
+		price: "875,000",
+		address1: "601 Country Club Dr",
+		city: "Carmel Valley",
+		state: "CA",
+		zip: "93924",
+		active: true,
+		image_file_name: "601_country_club.jpg"
+		},
+	{
+		price: "350,000",
+		address1: "3069 Sunset Ave",
+		city: "Marina",
+		state: "CA",
+		zip: "93933",
+		active: true,
+		image_file_name: "3069_sunset.jpg"
+	},
+	{
+		price: "1,395,000",
+		address1: "25910 Canada Drive",
+		city: "Carmel",
+		state: "CA",
+		zip: "93923",
+		active: true,
+		image_file_name: "25910_canada_dr.jpg"
+	},
+	{
+		price: "850,000",
+		address1: "1430 Manor Drive",
+		city: "Monterey",
+		state: "CA",
+		zip: "93940",
+		active: true,
+		image_file_name: "1430_manor.jpg"
+	}
+])
